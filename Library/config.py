4b99a6d0691811ba7875430d63316418ca5797ab
vidyagowda159 <vidyagowda005@gmail.com>
2022-08-09 14:13:19 +0530

    Sharing the framework

---
+++ Library/config.py
@@ -0,0 +1,11 @@
+class Config:
+    URL = 'http://demowebshop.tricentis.com/'
+    BROWSER_NAME = "chrome"
+    USERNAME = ""
+    PASSWORD = ""
+    FIREFOX_DRIVER_PATH = r""
+    GECKO_DRIVER_PATH = r""
+    CHROME_DRIVER_PATH = r"C:\Users\Vidyashree M C\PycharmProjects\Python_Selenium_FW\Python_Selenium_FW-master\Library\chromedriver"
+    IE_DRIVER_PATH = r""
+    DATA_FILE_PATH = r"C:\Users\Vidyashree M C\PycharmProjects\Python_Selenium_FW\Python_Selenium_FW-master\Data\TestData.xls"
+    OBJECTS_FILE_PATH = r"C:\Users\Vidyashree M C\PycharmProjects\Python_Selenium_FW\Python_Selenium_FW-master\Data\Objects.xls"
